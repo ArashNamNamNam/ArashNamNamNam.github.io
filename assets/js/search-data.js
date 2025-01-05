b8d32720924005d8ee6b051d83ed9a87f250d4be
ArashNamNamNam <arash.amiri1385@gmail.com>
2025-01-05 17:32:27 +0000

    Deploying to gh-pages from @ ArashNamNamNam/ArashNamNamNam.github.io@6c838e3483323923f415bc2c01a8ebf483a27d65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,17 +94,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/advanced-images/";
         
       },
-    },{id: "post-a-post-with-chart-js",
-      
-        title: "a post with chart.js",
-      
-      description: "this is what included chart.js code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/chartjs/";
-        
-      },
     },{id: "post-a-post-with-tikzjax",
       
         title: "a post with TikZJax",
